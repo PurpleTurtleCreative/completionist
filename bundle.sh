966f15e68756844277f9032be025598c89f69f4f
Michelle Blanchette <michelle@purpleturtlecreative.com>
2020-12-29 08:38:15 -0500

    fix bundle excludes

--- bundle.sh
+++ bundle.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 VERSION=$(grep -Eio 'Version:\s*[0-9\.]+' completionist.php | grep -Eo '[0-9\.]+')
 
-# zip -rT9X ../completionist.zip ./ --exclude '*.git*' '*.DS_Store' '*.zip' '*.log' '*.map' '*.sh' README.md composer.json composer.lock composer.phar '*node_modules/*' package-lock.json package.json terser.json 'assets/css/scss/*'
-
 cd ..
-touch .tmp
-zip -rT9X completionist.zip .tmp completionist/ --exclude '*/.git*' '*/.DS_Store' '*.zip' '*.log' '*.map' '*.sh' README.md composer.json composer.lock composer.phar '*/node_modules/*' package-lock.json package.json terser.json 'assets/css/scss/*'
-zip -d completionist.zip .tmp
-rm -f .tmp
+zip -rT9X completionist.zip completionist/ --exclude '*/.git*' '*/.DS_Store' '*.zip' '*.log' '*.map' '*.sh' completionist/README.md completionist/composer.json completionist/composer.lock 'completionist/node_modules/*' completionist/package-lock.json completionist/package.json completionist/terser.json 'completionist/assets/css/scss/*'
 
 echo "Bundled v${VERSION}."
