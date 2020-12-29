79031299f46f34b0ac6334cb4a9ec91e0b22750b
Michelle Blanchette <michelle@purpleturtlecreative.com>
2020-12-29 08:14:25 -0500

    fix bundle exclusions

--- bundle.sh
+++ bundle.sh
@@ -5,7 +5,7 @@ VERSION=$(grep -Eio 'Version:\s*[0-9\.]+' completionist.php | grep -Eo '[0-9\.]+
 
 cd ..
 touch .tmp
-zip -rT9X completionist.zip .tmp completionist/ --exclude '*.git*' '*.DS_Store' '*.zip' '*.log' '*.map' '*.sh' README.md composer.json composer.lock composer.phar '*node_modules/*' '*vendor/*' package-lock.json package.json terser.json 'assets/css/scss/*'
+zip -rT9X completionist.zip .tmp completionist/ --exclude '*/.git*' '*/.DS_Store' '*.zip' '*.log' '*.map' '*.sh' README.md composer.json composer.lock composer.phar '*/node_modules/*' package-lock.json package.json terser.json 'assets/css/scss/*'
 zip -d completionist.zip .tmp
 rm -f .tmp
 
