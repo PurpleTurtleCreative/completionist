62e38dced33cbd4f480ee4e329166e98d335292f
Michelle Blanchette <michelle@purpleturtlecreative.com>
2021-01-01 18:54:00 -0500

    ENH: Add semver tag to bundle

--- bundle.sh
+++ bundle.sh
@@ -2,6 +2,4 @@
 VERSION=$(grep -Eio 'Version:\s*[0-9\.]+' completionist.php | grep -Eo '[0-9\.]+')
 
 cd ..
-zip -rT9X completionist.zip completionist/ --exclude '*/.git*' '*/.DS_Store' '*.zip' '*.log' '*.map' '*.sh' completionist/README.md completionist/composer.json completionist/composer.lock 'completionist/node_modules/*' completionist/package-lock.json completionist/package.json completionist/terser.json 'completionist/assets/css/scss/*'
-
-echo "Bundled v${VERSION}."
+zip -rT9X completionist-"${VERSION}".zip completionist --exclude '*/.git*' '*/.DS_Store' '*.zip' '*.log' '*.map' '*.sh' completionist/README.md completionist/composer.json completionist/composer.lock 'completionist/node_modules/*' completionist/package-lock.json completionist/package.json completionist/terser.json 'completionist/assets/css/scss/*'
