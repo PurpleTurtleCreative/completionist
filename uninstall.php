dccbdc9ae7fe7a07be35b759cbb41cd29eb6c9a0
Michelle Blanchette <michelle@purpleturtlecreative.com>
2021-07-26 23:12:14 -0400

    fix src file paths for uninstall

--- uninstall.php
+++ uninstall.php
@@ -13,38 +13,37 @@ defined( 'WP_UNINSTALL_PLUGIN' ) || die();
 
 /* Delete all plugin options */
 
-include_once __DIR__ . '/src/class-options.php';
-include_once __DIR__ . '/src/class-database-manager.php';
+include_once __DIR__ . '/src/includes/class-options.php';
+include_once __DIR__ . '/src/includes/class-database-manager.php';
 
 if ( function_exists( 'get_sites' ) ) {
-  $site_ids = get_sites( [ 'fields' => 'ids' ] );
-  foreach ( $site_ids as $site_id ) {
-    switch_to_blog( $site_id );
-    uninstall_for_current_blog();
-    restore_current_blog();
-  }//end foreach $site_ids
+	$site_ids = get_sites( [ 'fields' => 'ids' ] );
+	foreach ( $site_ids as $site_id ) {
+		switch_to_blog( $site_id );
+		uninstall_for_current_blog();
+		restore_current_blog();
+	}//end foreach $site_ids
 } else {
-  uninstall_for_current_blog();
+	uninstall_for_current_blog();
 }
 
 /* HELPERS */
 
 function uninstall_for_current_blog() {
 
-  if ( class_exists( __NAMESPACE__ . '\Options' ) ) {
-    if ( method_exists( __NAMESPACE__ . '\Options', 'delete_all' ) ) {
-      Options::delete_all();
-    }
-  }
+	if ( class_exists( __NAMESPACE__ . '\Options' ) ) {
+		if ( method_exists( __NAMESPACE__ . '\Options', 'delete_all' ) ) {
+			Options::delete_all();
+		}
+	}
 
-  if ( class_exists( __NAMESPACE__ . '\Database_Manager' ) ) {
-    if (
-      method_exists( __NAMESPACE__ . '\Database_Manager', 'init' )
-      && method_exists( __NAMESPACE__ . '\Database_Manager', 'drop_all_tables' )
-    ) {
-      Database_Manager::init();
-      Database_Manager::drop_all_tables();
-    }
-  }
-
+	if ( class_exists( __NAMESPACE__ . '\Database_Manager' ) ) {
+		if (
+			method_exists( __NAMESPACE__ . '\Database_Manager', 'init' )
+			&& method_exists( __NAMESPACE__ . '\Database_Manager', 'drop_all_tables' )
+		) {
+			Database_Manager::init();
+			Database_Manager::drop_all_tables();
+		}
+	}
 }
