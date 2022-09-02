6dc12947dbac4a4976143fcde79e6eb7ca3f3c56
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-09-02 14:12:02 -0400

    #54 - ensure NoteBox styles are imported for when Asana authentication has not yet been provided

--- build/index_BlockEditor.jsx.asset.php
+++ build/index_BlockEditor.jsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'dea655c1460fff41c3d4');
+<?php return array('dependencies' => array('wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'e49c0f6db5f1d6b3d2e7');
