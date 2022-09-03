f6cdac2bd789680c7de40d86fb5a5c05b3885cf8
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-09-02 22:08:38 -0400

    rm unnecessary console logs

--- build/index_BlockEditor.jsx.asset.php
+++ build/index_BlockEditor.jsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '52f0613cf4200ff58e5b');
+<?php return array('dependencies' => array('wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '57fa00fda92e57171517');
--- build/index_DashboardWidget.jsx.asset.php
+++ build/index_DashboardWidget.jsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '24c006c956145ef1aebc');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'b9f8857d98b8d093aec7');
