793279d175b804aaf0b1ee0012d41a9eec4cd31d
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-10-05 16:28:26 -0400

    #85 - confirmed NoticesContainer and addNotices for Pinned Tasks editor panel

--- src/components/notice/NoticesContainer.jsx
+++ src/components/notice/NoticesContainer.jsx
@@ -2,14 +2,10 @@ import { NoticeContext } from './NoticeContext.jsx';
 
 import '/assets/styles/scss/components/notice/_NoticesContainer.scss';
 
-const { useContext, useEffect } = wp.element;
+const { useContext } = wp.element;
 
 export default function NoticesContainer() {
 	const { addNotice, getRenderedNotices } = useContext(NoticeContext);
-
-	useEffect(() => {
-		addNotice( 'The NoticesContainer just mounted!' );
-	}, []);
 
 	return (
 		<div className={"ptc-NoticesContainer"}>
