d4d326390a31d15b8a69a0b36bf28ced1b5fd4f1
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-10-27 14:36:46 -0400

    #103 - render toggle and expanded content for TaskListItem, not yet styled

--- src/components/task/TaskListItem.jsx
+++ src/components/task/TaskListItem.jsx
@@ -2,31 +2,39 @@ import { countIncompleteTasks } from './util';
 
 import { ReactComponent as CheckmarkIcon } from '/assets/icons/fa-check-solid.svg';
 import { ReactComponent as SubtasksIcon } from '/assets/icons/fa-code-branch-solid.svg';
+import { ReactComponent as ToggleIcon } from '/assets/icons/fa-caret-right-solid.svg';
 
 import '/assets/styles/scss/components/task/_TaskListItem.scss';
 
 const { useState } = wp.element;
 
 export default function TaskListItem({ task, rowNumber = null }) {
+	const [ isExpanded, setIsExpanded ] = useState(false);
 
 	let extraClassNames = '';
+
+	let renderToggle = false;
+	let showToggle = false;
 
 	let maybeCompleted = null;
 	if ( 'completed' in task ) {
 		let label = 'Incomplete';
 		if ( true === task.completed ) {
-			extraClassNames += ' --is-complete';
+			extraClassNames += ' --is-completed';
 			label = 'Completed';
 		}
 		maybeCompleted = (
 			<div className="completed" data-completed={task.completed}>
-				<CheckmarkIcon title={label} preserveAspectRatio="xMidYMid meet" />
+				<CheckmarkIcon aria-label={label} preserveAspectRatio="xMidYMid meet" />
 			</div>
 		);
 	}
 
 	let maybeSubtaskCount = null;
+	let maybeSubtaskList = null;
 	if ( 'subtasks' in task ) {
+
+		renderToggle = true;
 
 		let maybeSubtaskContent = null;
 		if (
@@ -34,12 +42,24 @@ export default function TaskListItem({ task, rowNumber = null }) {
 			Array.isArray( task.subtasks )
 		) {
 			const incompleteSubtasksCount = countIncompleteTasks(task.subtasks);
-			if ( incompleteSubtasksCount > 0 ){
+			if ( incompleteSubtasksCount > 0 ) {
+				showToggle = true;
 				maybeSubtaskContent = (
 					<>
 						{incompleteSubtasksCount}
-						<SubtasksIcon title="Subtasks" style={{ "transform": 'rotate(90deg)' }} preserveAspectRatio="xMidYMid meet" />
+						<SubtasksIcon aria-label="Subtasks" style={{ "transform": 'rotate(90deg)' }} preserveAspectRatio="xMidYMid meet" />
 					</>
+				);
+				maybeSubtaskList = (
+					<ul className="subtasks">
+						{
+							task.subtasks.map((subtask, index) => {
+								// Remove "subtasks" to prevent infinite recursion.
+								const { subtasks, ...task } = subtask;
+								return <TaskListItem task={task} rowNumber={index+1} />;
+							})
+						}
+					</ul>
 				);
 			}
 		}
@@ -48,8 +68,8 @@ export default function TaskListItem({ task, rowNumber = null }) {
 	}
 
 	let maybeName = null;
-	if ( 'name' in task && task.name ) {
-		maybeName = <p className="task-name">{task.name}</p>;
+	if ( 'name' in task ) {
+		maybeName = <p className="task-name">{task.name ?? ''}</p>;
 	}
 
 	let maybeAssignee = null;
@@ -88,7 +108,7 @@ export default function TaskListItem({ task, rowNumber = null }) {
 	if ( 'due_on' in task ) {
 
 		let maybeDueDateString = null;
-		if ( 'due_on' in task && task.due_on ) {
+		if ( task.due_on ) {
 			maybeDueDateString = new Date(task.due_on).toLocaleDateString(
 				undefined,
 				{
@@ -102,15 +122,50 @@ export default function TaskListItem({ task, rowNumber = null }) {
 		maybeDueDate = <p className="due">{maybeDueDateString}</p>;
 	}
 
+	let maybeDescription = null;
+	if ( 'html_notes' in task ) {
+		renderToggle = true;
+		if ( task.html_notes ) {
+			showToggle = true;
+			maybeDescription = (
+			<div
+				className="description"
+				dangerouslySetInnerHTML={ { __html: task.html_notes } }
+			/>
+		);
+		}
+	}
+
+	let maybeToggle = null;
+	if ( renderToggle ) {
+		let maybeToggleIcon = null;
+		if ( showToggle ) {
+			maybeToggleIcon = <ToggleIcon preserveAspectRatio="xMidYMid meet" onClick={() => setIsExpanded(!isExpanded)} />
+		}
+		maybeToggle = <div className="toggle">{maybeToggleIcon}</div>;
+	}
+
+	let maybeExpandedDetails = null;
+	if ( isExpanded ) {
+		maybeExpandedDetails = (
+			<div className="expanded-details">
+				{maybeDescription}
+				{maybeSubtaskList}
+			</div>
+		);
+	}
+
 	return (
 		<li className={"ptc-TaskListItem"+extraClassNames}>
 			{ rowNumber && <div className="row-number">{rowNumber}</div> }
+			{maybeToggle}
 			{maybeCompleted}
 			<div className="body">
 				{maybeName}
 				{maybeSubtaskCount}
 				{maybeAssignee}
 				{maybeDueDate}
+				{maybeExpandedDetails}
 			</div>
 		</li>
 	);
