446e1ea7eba02f492e648cfa3beef892620f4405
Michelle Blanchette <michelle@purpleturtlecreative.com>
2023-12-10 15:14:56 -0500

    update bundle script to export zip file abspath in GitHub Actions

--- bundle.sh
+++ bundle.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+# Determine package values.
+
 PLUGIN_SLUG=$( basename `pwd` )
 
-VERSION=$(grep -Eio 'Version:\s*[0-9\.]+' "${PLUGIN_SLUG}.php" | grep -Eo '[0-9\.]+')
+VERSION=$(grep -Eio 'Version:\s*\d+\.\d+\.\d+(\-rc\.\d+)?' "${PLUGIN_SLUG}.php" | grep -Eo '\d+\.\d+\.\d+(\-rc\.\d+)?')
 
-cd ..
-zip -rT9X "${PLUGIN_SLUG}-${VERSION}.zip" "${PLUGIN_SLUG}" --exclude @"${PLUGIN_SLUG}"/exclude.lst
+# Create the plugin package.
+
+pushd ..
+
+PLUGIN_ZIP_FILE=$( pwd )/"${PLUGIN_SLUG}-${VERSION}.zip"
+
+zip -rT9X "${PLUGIN_ZIP_FILE}" "${PLUGIN_SLUG}" --exclude @"${PLUGIN_SLUG}"/exclude.lst
+
+popd
+
+# Maybe export variables for GitHub Action.
+
+if [ -n "$GITHUB_ENV" ]; then
+  echo "PTC_PLUGIN_ZIP_FILE=${PLUGIN_ZIP_FILE}" >> "$GITHUB_ENV"
+  echo "Exported GitHub ENV variables: PTC_PLUGIN_ZIP_FILE"
+else
+  echo "Did not set \$GITHUB_ENV variables."
+fi
+
+# Final notes.
+
+echo
+echo "!! Finished bundling plugin package: ${PLUGIN_ZIP_FILE}"
+echo
