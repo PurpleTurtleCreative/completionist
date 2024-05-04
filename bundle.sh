af2a296ea21576cd8249428d81d30227bc1d408a
Michelle Blanchette <michelle@purpleturtlecreative.com>
2024-05-04 21:03:13 +0000

    note about package being contaminated if bundled in a local dev environment

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# === IMPORTANT NOTICE for LOCAL DEV ENVIRONMENT ===
+#
+# Running this bundler is not exactly proper in a local development
+# environment that has dev dependencies already installed.
+# The best way to run this bundler is through the GitHub Actions
+# `publish.yml` pipeline. Your local environment may contain files
+# that are not explicitly defined in `.distignore`. We do not know
+# what files you have locally that would not otherwise be included
+# on a clean installation.
+#
+# If you absolutely must run this on your own machine, follow the
+# pipeline's steps in a fresh folder.
+
 # Determine package values.
 
 PLUGIN_SLUG=$( basename `pwd` )
