2c2e4f914f7829d85022fdf519467c3e1ae98989
Michelle Blanchette <33374343+MichelleBlanchette@users.noreply.github.com>
2024-05-04 09:36:16 -0400

    install php-asana fork to update composer dependencies, resolves #217 (#224)

--- build_install.sh
+++ build_install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Currently, only dev dependencies are included,
+# so installation is unnecessary.
+# composer install --optimize-autoloader --no-dev
+
+# Install our own fork of Asana's PHP SDK because
+# they haven't pushed updates in a very long time
+# and we include our own repairs for PHP 8 compatibility.
+pushd php-asana
 composer install --optimize-autoloader --no-dev
+popd
 
 npm ci --no-audit
 npm run build
